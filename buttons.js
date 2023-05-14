d22642d39c794e7836ad7dff82f5f30302459f73
Jeroen-dH <proskater7437@gmail.com>
2023-05-14 14:39:57 +0200

    done

--- buttons.js
+++ buttons.js
@@ -1,4 +1,5 @@
 var aantalBoxen = 30;
+colors = ["green","red","purple","blue","black"]
 
 document.write("<div style='display: grid;gap: 5px 5px;grid-template-columns:auto auto auto auto auto;background-color:grey;'>");
 for (let index = 1; index <= aantalBoxen; index++) {
@@ -9,20 +10,26 @@ for (let index = 1; index <= aantalBoxen; index++) {
 document.write("</div>");
 
 function ChangeBGC(id){
-    switch (document.getElementById(id).style.backgroundColor){
-        case "green":
-            document.getElementById(id).style.backgroundColor = "red";
-            break
-        case "red":
-            document.getElementById(id).style.backgroundColor = "purple";
-            break
-        case "purple":
-            document.getElementById(id).style.backgroundColor = "blue";
-            break
-        case "blue":
-            document.getElementById(id).style.visibility = "hidden";
-            break
-    }
-    
-
+    kleur = document.getElementById(id).style.backgroundColor;
+    console.log(kleur);
+    console.log(colors[1]);
+    colors.forEach(element => {
+        switch(kleur){
+            case colors[0]:
+                document.getElementById(id).style.backgroundColor = colors[1];
+                break
+            case colors[1]:
+                document.getElementById(id).style.backgroundColor = colors[2];
+                break
+            case colors[2]:
+                document.getElementById(id).style.backgroundColor = colors[3];
+                break
+            case colors[3]:
+                document.getElementById(id).style.backgroundColor = colors[4];
+                break
+            case colors[4]:
+                document.getElementById(id).style.visibility = "hidden";
+                break
+        }
+    });
 }
